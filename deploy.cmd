9382988bf3aacec5ec55f715e9a525666e1947cc
antizen1111111 <2385407157@qq.com>
2019-01-09 18:34:42 +0800

    'submit'

--- deploy.cmd
+++ deploy.cmd
@@ -1,25 +1,26 @@
-# 进入当前目录
+:: 进入当前目录
 cd %~dp0
 
-# 删除多余的文件
+:: 删除多余的文件
 rd /s/q node_modules
 rd /s/q docs\\.vuepress\\dist
 
-# 将源码上传至github
+:: 将源码上传至github
 git init
 git add -A
-git commit -m '提交源文件'
+git commit -m 'submit'
 
 git push -f git@github.com:forwardfirst/antizen-note.git master
 
 
-# 生成静态文件
+:: 生成静态文件
 npm run build
 
-# 进入生成的文件夹
+:: 进入生成的文件夹
 cd docs/.vuepress/dist
 
-# 上传至静态展示github
+:: 上传至静态展示github
+
 git init
 git add -A
 git commit -m 'deploy'
